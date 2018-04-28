be6edbdfcda4d40b6e92b3f0d656a3a08da1e5a1
Darshini-V-M <37341431+Darshini-V-M@users.noreply.github.com>
2018-04-28 15:56:44 +0530

    Create print holiday or not.py

---
+++ player level/print holiday or not.py
@@ -0,0 +1,17 @@
+day= raw_input()
+if(day=="Monday"):
+	print "no"
+elif(day=="Tueday"):
+	print "no"	
+elif(day=="Wednesday"):
+	print "no"
+elif(day=="Thursday"):
+	print "no"	
+elif(day=="Friday"):
+	print "no"
+elif(day=="Saturday"):
+	print "yes"
+elif(day=="Sunday"):
+	print "yes"	
+else:
+	print"enter valid"
